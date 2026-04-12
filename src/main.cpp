6817ce30cbc1840f7a217fb3d5377a50690271c8
Samarth <samarth@Mac.alphionsee.in>
2026-04-12 10:18:55 +0530

    adding output control system

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,12 @@
 #include "TempManager.h"
 #include "WeightManager.h"
 #include "PowerMonitor.h"
-#include "ActuatorController.h"
 #include "pins.h"
 
 
 TempManager temps(TEMP_BUS_PIN);
 WeightManager weight;
 PowerMonitor power(Serial2, 16, 17); // RX, TX
-ActuatorController control(18, 19, 20, 21, 22, 23); // Relay, V1, V2, V3, LA_U, LA_D
 
 // ESP32 usually has the internal LED on GPIO 2
 const int ledPin = 2;
@@ -18,7 +16,6 @@ void setup() {
     Serial.begin(115200);
     temps.begin();
     weight.begin(LOADCELL_DOUT, LOADCELL_SCK);
-    control.begin();
 }
 
 void loop() {
@@ -29,7 +26,6 @@ void loop() {
     power.readData(p, f);
 
     // 2. Logic (Example: Stop motor if weight > 10kg)
-    if (w > 10.0) control.stopMotor();
 
     delay(500);
 }
